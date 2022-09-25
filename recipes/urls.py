ba4fef831e77a438134648859224fb31405105e0
lucenasoft <getugbr@gmail.com>
2022-09-25 20:55:28 -0300

    Removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from recipes.views import home, contato, sobre
+from recipes.views import home
 #  HTTP REQUEST
 
     # HTTP REQUEST
 urlpatterns = [
     path('', home), #home
-    path('sobre/', sobre), # /sobre/
-    path('contato/', contato) # /contato/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,9 +4,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
