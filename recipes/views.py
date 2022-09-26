17127e84d804a32080a69d366a83cd748c7f96d7
lucenasoft <getugbr@gmail.com>
2022-09-25 21:57:58 -0300

    Add header

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
