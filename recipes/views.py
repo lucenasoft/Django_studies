d98f830636ad948d7990f1bd26e12373591042fe
lucenasoft <getugbr@gmail.com>
2022-09-29 17:44:13 -0300

    add_factory_manager

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from utils.factory import make_recipe
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(6)],
+        'is_descripit_block': True,
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_datail_page': True,
+    })
