125942407d9e3e464815a56500ea89bb4c347cf4
lucenasoft <getugbr@gmail.com>
2022-09-29 19:49:14 -0300

    add name_unique_urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 #  HTTP REQUEST
 
+app_name = 'recipes'
+
     # HTTP REQUEST
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe) #home
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe") #home
 ]
